23350fff38dc02e27e04a7af68d7ea7bc57c14e0
Liang Bo <liangbo@Liangs-MacBook-Pro.local>
2026-06-03 11:10:54 +0800

    chore: release soxbot v3.49.1

--- Casks/soxbot.rb
+++ Casks/soxbot.rb
@@ -1,6 +1,6 @@
 cask "soxbot" do
   version "3.49.1"
-  sha256 "3e47d5f9fa2282d5b3444ef7c2aede411836d295edb991f62b16d69ac62f30d8"
+  sha256 "20d4ab98ee3846434c65f50c8181eb0252c8e0ed5a4432b82cb5f248c72fc53c"
 
   url "https://cdn.soxai.io/sox.bot/releases/v#{version}/sox.bot_#{version}_aarch64.dmg"
   name "sox.bot"
